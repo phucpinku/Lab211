d99173a46dec98faf558c6a449df8b69b200e835
phucpinku <hongphuchong456@gmail.com>
2024-09-16 08:42:31 +0700

    Update RAMItems.java

--- LaptopRAMManagement/src/laptoprammanagement/RAMItems.java
+++ LaptopRAMManagement/src/laptoprammanagement/RAMItems.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package laptoprammanagement;
 
-/**
- *
- * @author ACER
- */
 import java.time.YearMonth;
 
 public class RAMItems implements java.io.Serializable {
